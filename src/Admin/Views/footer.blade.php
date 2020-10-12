dd95968f0b04e08b0c2cedf9a2fb5c4f33a388c3
Lanh Le <lanhktc@gmail.com>
2020-10-13 06:09:32 +0700

    no message

--- src/Admin/Views/footer.blade.php
+++ src/Admin/Views/footer.blade.php
@@ -1,11 +1,7 @@
 <footer class="main-footer">
   <div class="float-right d-none d-sm-inline-block">
     <b>Version</b> 
-    @if (env('SC_VERSION'))
-    {{ env('SC_VERSION') }}
-    @else
-    {{ config('s-cart.sub_version') }}
-    @endif
+    {{ config('s-cart.version') }}
   </div>
   <strong>Copyright &copy; {{ date('Y') }} <a href="{{ config('s-cart.homepage') }}">SCart: {{ config('s-cart.title') }}</a>.</strong> All rights
   reserved.
