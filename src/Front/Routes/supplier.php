9b3e1ce378cf9d545381d5598835301fff79fc49
Naruto <lanhktc@gmail.com>
2020-11-05 14:09:00 +0700

    Remove front supplier

--- src/Front/Routes/supplier.php
+++
@@ -1,8 +0,0 @@
-<?php
-$prefixSupplier = sc_config('PREFIX_SUPPLIER')??'supplier';
-
-Route::group(['prefix' => $prefixSupplier], function ($router) use($suffix) {
-    $router->get('/', 'ShopSupplierController@allSuppliers')->name('supplier.all');
-    $router->get('/{alias}'.$suffix, 'ShopSupplierController@supplierDetail')
-        ->name('supplier.detail');
-});
