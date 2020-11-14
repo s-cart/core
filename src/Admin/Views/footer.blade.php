44e8086b730aa06b3c5c75f1535b4f8cff9df579
Lanh Le <lanhktc@gmail.com>
2020-11-14 22:48:56 +0700

    Update view admin

--- src/Admin/Views/footer.blade.php
+++ src/Admin/Views/footer.blade.php
@@ -1,8 +1,10 @@
 <footer class="main-footer">
-  <div class="float-right d-none d-sm-inline-block">
-    <b>Version</b> 
-    {{ config('s-cart.sub-version') }}
-  </div>
-  <strong>Copyright &copy; {{ date('Y') }} <a href="{{ config('s-cart.homepage') }}">SCart: {{ config('s-cart.title') }}</a>.</strong> All rights
-  reserved.
+  @if (!sc_config_admin('ADMIN_FOOTER_OFF'))
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 
+      {{ config('s-cart.sub-version') }}
+    </div>
+    <strong>Copyright &copy; {{ date('Y') }} <a href="{{ config('s-cart.homepage') }}">SCart: {{ config('s-cart.title') }}</a>.</strong> All rights
+    reserved.
+  @endif
 </footer>
