8c1f42f81781faec4ccb3bb06dc4331b85aeb952
Lanh Le <lanhktc@gmail.com>
2020-11-14 22:16:33 +0700

    no message

--- src/Admin/Views/footer.blade.php
+++ src/Admin/Views/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
   <div class="float-right d-none d-sm-inline-block">
     <b>Version</b> 
-    {{ config('s-cart.version') }}
+    {{ config('s-cart.sub-version') }}
   </div>
   <strong>Copyright &copy; {{ date('Y') }} <a href="{{ config('s-cart.homepage') }}">SCart: {{ config('s-cart.title') }}</a>.</strong> All rights
   reserved.
