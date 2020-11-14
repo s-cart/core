7ead768f24a23a8a8a96a6eada3bb084290c9d85
Lanh Le <lanhktc@gmail.com>
2020-11-14 16:27:02 +0700

    no message

--- src/Config/s-cart.php
+++ src/Config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'version'     => '4.2',
-    'sub-version' => '4.2.2',
+    'version'     => '4.3',
+    'sub-version' => '4.3.0-beta',
     'homepage'    => 'https://s-cart.org',
     'name'        => 'S-Cart',
     'title'       => 'Free Open Source eCommerce for Business',
