0033e1dff329496f3ac32fd8cdafb37a7b806457
Lanh Le <lanhktc@gmail.com>
2020-11-14 22:11:21 +0700

    no message

--- src/Admin/Views/screen/list.blade.php
+++ src/Admin/Views/screen/list.blade.php
@@ -118,10 +118,12 @@
       </div>
 
 
-
       <!-- /.card-header -->
       <div class="card-body p-0" id="pjax-container">
-        <div id="url-sort" data-urlsort="{!! strpos($urlSort ?? '', "?")?$urlSort."&":$urlSort."?" !!}"  style="display: none;"></div>
+        @php
+            $urlSort = $urlSort ?? '';
+        @endphp
+        <div id="url-sort" data-urlsort="{!! strpos($urlSort, "?")?$urlSort."&":$urlSort."?" !!}"  style="display: none;"></div>
         <div class="table-responsive">
         <table class="table table-hover box-body text-wrap table-bordered">
           <thead>
