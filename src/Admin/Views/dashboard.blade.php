05b4f969432b20d275efebe53941a9cb7fc7478a
Lanh Le <lanhktc@gmail.com>
2020-11-29 23:01:09 +0700

    Update admin

--- src/Admin/Views/dashboard.blade.php
+++ src/Admin/Views/dashboard.blade.php
@@ -12,17 +12,10 @@
               <div class="info-box-content">
                 <span class="info-box-text">{{ trans('admin.total_order') }}</span>
                 <span class="info-box-number">{{ number_format($totalOrder) }}</span>
-                @if (sc_config_global('MultiStorePro'))
-                  <a href="{{ sc_route('admin_order_store.index') }}" class="small-box-footer">
-                    {{ trans('admin.more') }}&nbsp;
-                    <i class="fa fa-arrow-circle-right"></i>
-                  </a>
-                @else
-                  <a href="{{ sc_route('admin_order.index') }}" class="small-box-footer">
-                    {{ trans('admin.more') }}&nbsp;
-                    <i class="fa fa-arrow-circle-right"></i>
-                  </a>
-                @endif
+                <a href="{{ sc_route('admin_order.index') }}" class="small-box-footer">
+                  {{ trans('admin.more') }}&nbsp;
+                  <i class="fa fa-arrow-circle-right"></i>
+                </a>
               </div>
               <!-- /.info-box-content -->
             </div>
@@ -66,8 +59,7 @@
             <!-- /.info-box -->
           </div>
           <!-- /.col -->
-  
-  
+    
   
           <!-- /.col -->
           <div class="col-md-3 col-sm-6 col-xs-12">
