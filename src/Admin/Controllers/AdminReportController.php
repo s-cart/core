213c9230771afc5983ba1ed3a79c4981c9be9d81
Lanh Le <lanhktc@gmail.com>
2020-12-19 21:50:40 +0700

    Fix page report

--- src/Admin/Controllers/AdminReportController.php
+++ src/Admin/Controllers/AdminReportController.php
@@ -121,7 +121,7 @@ class AdminReportController extends RootAdminController
         foreach ($arrSort as $key => $status) {
             $optionSort .= '<option  ' . (($sort_order == $key) ? "selected" : "") . ' value="' . $key . '">' . $status . '</option>';
         }
-        $data['urlSort'] = sc_route('admin_report.index', request()->except(['_token', '_pjax', 'sort_order']));
+        $data['urlSort'] = sc_route('admin_report.product', request()->except(['_token', '_pjax', 'sort_order']));
 
         $data['optionSort'] = $optionSort;
         //=menuSort
