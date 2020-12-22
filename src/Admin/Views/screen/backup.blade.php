f8ae4faa65730caa919c435493020684f4cefff9
Lanh Le <lanhktc@gmail.com>
2020-12-22 22:15:07 +0700

    Backup

--- src/Admin/Views/screen/backup.blade.php
+++ src/Admin/Views/screen/backup.blade.php
@@ -2,14 +2,51 @@
 
 @section('main')
       <div class="row">
-        <div class="col-xs-12">
+        <div class="col-12">
           <div class="card">
             <div class="card-header">
               <h3 class="card-title">{{ $title }}</h3>
             </div>
             <!-- /.card-header -->
             <div class="card-body"  id="pjax-container">
-              <div><button id="generate" onClick="generateBackup($(this));" class="btn btn-success btn-flat" data-loading-text="{{ trans('backup.processing') }}"><span class="glyphicon glyphicon-plus"></span>{{ trans('backup.generate_now') }}</button></div>
+
+              <div class="btn-group">
+                <button type="button" class="btn btn-default btn-flat"><i class="fa fa-save"></i> Backup</button>
+                <button type="button" class="btn btn-default btn-flat dropdown-toggle" data-toggle="dropdown" data-loading-text="{{ trans('backup.processing') }}">
+                  <span class="caret"></span>
+                  <span class="sr-only">Toggle Dropdown</span>
+                </button>
+                <ul class="dropdown-menu keep-open" role="menu">
+                  <li style="height: 200px; overflow:auto; padding:10px;">
+                    <table>
+                      <tr><th></th><th>Table</th><th>Rows</th><th>Size</th></tr>
+                      @foreach ($tableInfo as $table)
+                      <tr>
+                        <td class="checkbox icheck">
+                            <input type="checkbox"  class="column-select-item table-process" data-id="{{ $table->table_name }}" checked/>
+                        </td>
+                        <td>
+                          {{ $table->table_name }}
+                        </td>
+                        <td>
+                          {{ $table->table_rows }}
+                        </td>
+                        <td>
+                          {{ number_format($table->data_length/1048576, 2) }}MB
+                        </td>
+                      </tr>
+                    @endforeach
+                    </table>
+                  </li>
+                  <li class="divider" style="border-top: 1px solid #eaeae1; padding-bottom: 5px;"></li>
+                  <li class="text-right">
+                      <button class="btn btn-sm btn-default column-select-all">All</button>&nbsp;&nbsp;
+                      <button class="btn btn-sm btn-success column-select-submit" onClick="generateBackup($(this));" id="generate">Submit</button>
+                  </li>
+              </ul>
+              </div>
+
+              <div class="table-responsive">
               <table id="main-table" class="table table-hover box-body text-wrap table-bordered">
                 <thead>
                 <tr>
@@ -29,13 +66,14 @@
                      <td>{{ $file['time']}}</td>
                      <td>{{ $file['name']}}</td>
                      <td>{{ $file['size']}}</td>
-                      <td>{!! '<a href="?download='.$file['name'].'"><button title="'.trans('backup.download').'" data-loading-text="'.trans('backup.processing').'" class="btn btn-flat btn-primary"><span class="glyphicon glyphicon-save"></span> '.trans('backup.download').'</button ></a>' !!}</td>
+                      <td>{!! '<a href="?download='.$file['name'].'"><button title="'.trans('backup.download').'" data-loading-text="'.trans('backup.processing').'" class="btn btn-flat btn-primary"><i class="fas fa-download"></i> '.trans('backup.download').'</button ></a>' !!}</td>
                       <td>{!! '<button  onClick="processBackup($(this),\''.$file['name'].'\',\'remove\');" title="'.trans('backup.remove').'" data-loading-text="'.trans('backup.processing').'" class="btn btn-flat btn-danger"><span class="glyphicon glyphicon-trash"></span> '.trans('backup.remove').'</button >' !!}</td>
                       <td>{!! '<button  onClick="processBackup($(this),\''.$file['name'].'\',\'restore\');" title="'.trans('backup.restore').'" data-loading-text="'.trans('backup.processing').'" class="btn btn-flat btn-warning"><span class="glyphicon glyphicon-retweet"></span> '.trans('backup.restore').'</button >' !!}</td>
                     </tr>
                   @endforeach
                 </tbody>
               </table>
+              </div>
             </div>
             <!-- /.card-body -->
           </div>
@@ -102,12 +140,19 @@
   function generateBackup(obj) {
       $('#loading').show()
       obj.button('loading');
+      var selected = [];
+      $('.table-process:checked').each(function(){
+          selected.push($(this).data('id'));
+      });
+      var includeTables = selected.join();
+
       $.ajax({
         type: 'POST',
         dataType:'json',
         url: '{{ sc_route('admin.backup.generate') }}',
         data: {
           "_token": "{{ csrf_token() }}",
+          "includeTables": includeTables,
         },
         success: function (response) {
           console.log(response);
@@ -137,6 +182,11 @@
       if ($.support.pjax) {
         $.pjax.defaults.timeout = 2000; // time in milliseconds
       }
+    });
+
+    $('.column-select-all').on('click', function () {
+      $('.column-select-item').iCheck('check');
+      return false;
     });
 
 </script>
