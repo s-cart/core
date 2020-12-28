bfd383c23842d5c4b12f1c9ad40b8294f4f5a101
Naruto <lanhktc@gmail.com>
2020-12-28 10:00:12 +0700

    Update

--- src/Admin/Views/footer.blade.php
+++ src/Admin/Views/footer.blade.php
@@ -1,10 +1,13 @@
 <footer class="main-footer">
   @if (!sc_config_admin('ADMIN_FOOTER_OFF'))
     <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 
+      <strong>Env</strong>
+      {{ config('app.env') }}
+      &nbsp;&nbsp;
+      <strong>Version</strong> 
       {{ config('s-cart.sub-version') }}
     </div>
-    <strong>Copyright &copy; {{ date('Y') }} <a href="{{ config('s-cart.homepage') }}">SCart: {{ config('s-cart.title') }}</a>.</strong> All rights
+    <strong>Copyright &copy; {{ date('Y') }} <a href="{{ config('s-cart.homepage') }}">S-Cart: {{ config('s-cart.title') }}</a>.</strong> All rights
     reserved.
   @endif
 </footer>
