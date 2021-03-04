dfe1f276bc3081e7f848caf341a495821b661290
Lanh Le <lanhktc@gmail.com>
2021-03-04 20:51:31 +0700

    no message

--- src/Config/s-cart.php
+++ src/Config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'version'     => '5.0',
-    'sub-version' => '5.0.6',
+    'version'     => '5.1',
+    'sub-version' => '5.1.0',
     'homepage'    => 'https://s-cart.org',
     'name'        => 'S-Cart',
     'title'       => 'Free Open Source eCommerce for Business',
