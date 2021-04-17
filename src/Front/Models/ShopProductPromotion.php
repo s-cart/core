0c17cfad0563ebdbcf9e17a8bdb70dc0754df613
Lanh Le <lanhktc@gmail.com>
2021-04-17 07:31:13 +0700

    no message

--- src/Front/Models/ShopProductPromotion.php
+++ src/Front/Models/ShopProductPromotion.php
@@ -8,7 +8,7 @@ class ShopProductPromotion extends Model
 {
     public $table = SC_DB_PREFIX.'shop_product_promotion';
     protected $guarded    = [];
-    protected $primaryKey = ['product_id'];
+    protected $primaryKey = 'product_id';
     public $incrementing  = false;
     protected $connection = SC_CONNECTION;
 
