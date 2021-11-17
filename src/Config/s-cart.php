328dd312866b86144b43941dee2ad942708f70da
S-Cart <giaiphap247.com@gmail.com>
2021-11-18 05:24:16 +0700

    Hotfix error securoty download file

--- src/Config/s-cart.php
+++ src/Config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'core'             => '6.7',
-    'core-sub-version' => '6.7.1',
+    'core-sub-version' => '6.7.2',
     'homepage'         => 'https://s-cart.org',
     'name'             => 'S-Cart',
     'github'           => 'https://github.com/s-cart/s-cart',
