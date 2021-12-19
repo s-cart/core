c3291c182b6e953b8665bbe2ca10d3090b381d4b
S-Cart <giaiphap247.com@gmail.com>
2021-12-20 06:20:39 +0700

    Update core

---
+++ src/Commands/Update.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace SCart\Core\Commands;
+
+use Illuminate\Console\Command;
+use Throwable;
+use DB;
+use Illuminate\Support\Facades\Artisan;
+
+class Update extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'sc:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update S-Cart core"';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Artisan::call('db:seed', 
+        [
+            '--class' => 'DataDefaultSeeder',
+            '--force' => true
+        ]
+        );
+        Artisan::call('db:seed', 
+        [
+            '--class' => 'DataLocalleSeeder',
+            '--force' => true
+        ]
+        );
+    }
+}
