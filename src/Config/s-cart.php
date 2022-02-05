a549e6c684e3c56adf78b2e80d13068399c68f62
S-Cart <giaiphap247.com@gmail.com>
2022-02-05 22:13:43 +0700

    no message

--- src/Config/s-cart.php
+++ src/Config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'core'             => '6.8',
-    'core-sub-version' => '6.8.15',
+    'core-sub-version' => '6.8.16',
     'homepage'         => 'https://s-cart.org',
     'name'             => 'S-Cart',
     'github'           => 'https://github.com/s-cart/s-cart',
