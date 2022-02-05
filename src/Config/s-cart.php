40f71afcdf895e9183fab229dd1d32017b97a0c4
S-Cart <giaiphap247.com@gmail.com>
2022-02-05 22:19:47 +0700

    Fix validate email

--- src/Config/s-cart.php
+++ src/Config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'core'             => '6.6',
-    'core-sub-version' => '6.6.2',
+    'core-sub-version' => '6.6.2.8',
     'homepage'         => 'https://s-cart.org',
     'name'             => 'S-Cart',
     'github'           => 'https://github.com/s-cart/s-cart',
