b6ddf67b8e9db3783d55dd076f0e2d7aebff6631
S-Cart <giaiphap247.com@gmail.com>
2022-02-08 06:57:13 +0700

    Update 6.9-beta

--- src/Config/s-cart.php
+++ src/Config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'core'             => '6.8',
-    'core-sub-version' => '6.8.16',
+    'core'             => '6.9',
+    'core-sub-version' => '6.9.0-beta',
     'homepage'         => 'https://s-cart.org',
     'name'             => 'S-Cart',
     'github'           => 'https://github.com/s-cart/s-cart',
