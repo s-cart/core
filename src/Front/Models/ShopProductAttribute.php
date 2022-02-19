cdc5e63fbcf9cf3b43720f8253efa871d126d620
S-Cart <giaiphap247.com@gmail.com>
2022-02-19 18:20:52 +0700

    Fix error model product attribute

--- src/Front/Models/ShopProductAttribute.php
+++ src/Front/Models/ShopProductAttribute.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class ShopProductAttribute extends Model
 {
     use \SCart\Core\Front\Models\ModelTrait;
-    
+    public $timestamps    = false;
     public $table = SC_DB_PREFIX.'shop_product_attribute';
     protected $guarded = [];
     protected $connection = SC_CONNECTION;
