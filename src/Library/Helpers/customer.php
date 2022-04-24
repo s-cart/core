2a2836e56f2556a6c46ea677883c96a8698aa884
S-Cart <giaiphap247.com@gmail.com>
2022-04-25 06:55:58 +0700

    Customize function mapping data customer before insert

--- src/Library/Helpers/customer.php
+++ src/Library/Helpers/customer.php
@@ -96,26 +96,120 @@ if (!function_exists('sc_customer_sendmail_verify') && !in_array('sc_customer_se
 /**
  * Mapping data address of customer
  *
- * @param   [type]  $dataClean  [$dataClean description]
+ * @param   [type]  $dataRaw  [$dataRaw description]
  *
  * @return  [array]              [return description]
  */
 if (!function_exists('sc_customer_address_mapping') && !in_array('sc_customer_address_mapping', config('helper_except', []))) {
-    function sc_customer_address_mapping(array $dataClean)
+    function sc_customer_address_mapping(array $dataRaw)
     {
         $dataAddress = [
-            'first_name'      => $dataClean['first_name'] ?? '',
-            'last_name'       => $dataClean['last_name'] ?? '',
-            'first_name_kana' => $dataClean['first_name_kana'] ?? '',
-            'last_name_kana'  => $dataClean['last_name_kana'] ?? '',
-            'postcode'        => $dataClean['postcode'] ?? '',
-            'address1'        => $dataClean['address1'] ?? '',
-            'address2'        => $dataClean['address2'] ?? '',
-            'address3'        => $dataClean['address3'] ?? '',
-            'country'         => $dataClean['country'] ?? '',
-            'phone'           => $dataClean['phone'] ?? '',
+            'first_name'      => $dataRaw['first_name'] ?? '',
+            'last_name'       => $dataRaw['last_name'] ?? '',
+            'first_name_kana' => $dataRaw['first_name_kana'] ?? '',
+            'last_name_kana'  => $dataRaw['last_name_kana'] ?? '',
+            'postcode'        => $dataRaw['postcode'] ?? '',
+            'address1'        => $dataRaw['address1'] ?? '',
+            'address2'        => $dataRaw['address2'] ?? '',
+            'address3'        => $dataRaw['address3'] ?? '',
+            'country'         => $dataRaw['country'] ?? '',
+            'phone'           => $dataRaw['phone'] ?? '',
         ];
 
         return $dataAddress;
+    }
+}
+
+
+/**
+ * Mapping data customer before insert
+ *
+ * @param   [array]  $dataRaw  [$dataRaw description]
+ *
+ * @return  [array]              [return description]
+ */
+if (!function_exists('sc_customer_data_insert_mapping') && !in_array('sc_customer_data_insert_mapping', config('helper_except', []))) {
+    function sc_customer_data_insert_mapping(array $dataRaw)
+    {
+        $dataInsert = [
+            'first_name' => $dataRaw['first_name'] ?? '',
+            'email' => $dataRaw['email'],
+            'password' => bcrypt($dataRaw['password']),
+        ];
+        if (isset($dataRaw['status'])) {
+            $dataInsert['status']  = $dataRaw['status'];
+        }
+        if (sc_config('customer_lastname')) {
+            if (!empty($dataRaw['last_name'])) {
+                $dataInsert['last_name'] = $dataRaw['last_name'];
+            }
+        }
+        if (sc_config('customer_firstname_kana')) {
+            if (!empty($dataRaw['first_name_kana'])) {
+                $dataInsert['first_name_kana'] = $dataRaw['first_name_kana'];
+            }
+        }
+        if (sc_config('customer_lastname_kana')) {
+            if (!empty($dataRaw['last_name_kana'])) {
+                $dataInsert['last_name_kana'] = $dataRaw['last_name_kana'];
+            }
+        }
+        if (sc_config('customer_address1')) {
+            if (!empty($dataRaw['address1'])) {
+                $dataInsert['address1'] = $dataRaw['address1'];
+            }
+        }
+        if (sc_config('customer_address2')) {
+            if (!empty($dataRaw['address2'])) {
+                $dataInsert['address2'] = $dataRaw['address2'];
+            }
+        }
+
+        if (sc_config('customer_address3')) {
+            if (!empty($dataRaw['address3'])) {
+                $dataInsert['address3'] = $dataRaw['address3'];
+            }
+        }
+
+        if (sc_config('customer_phone')) {
+            if (!empty($dataRaw['phone'])) {
+                $dataInsert['phone'] = $dataRaw['phone'];
+            }
+        }
+        if (sc_config('customer_country')) {
+            if (!empty($dataRaw['country'])) {
+                $dataInsert['country'] = $dataRaw['country'];
+            }
+        }
+        if (sc_config('customer_postcode')) {
+            if (!empty($dataRaw['postcode'])) {
+                $dataInsert['postcode'] = $dataRaw['postcode'];
+            }
+        }
+        if (sc_config('customer_company')) {
+            if (!empty($dataRaw['company'])) {
+                $dataInsert['company'] = $dataRaw['company'];
+            }
+        }
+        if (sc_config('customer_sex')) {
+            if (!empty($dataRaw['sex'])) {
+                $dataInsert['sex'] = $dataRaw['sex'];
+            }
+        }
+        if (sc_config('customer_birthday')) {
+            if (!empty($dataRaw['birthday'])) {
+                $dataInsert['birthday'] = $dataRaw['birthday'];
+            }
+        }
+        if (sc_config('customer_group')) {
+            if (!empty($dataRaw['group'])) {
+                $dataInsert['group'] = $dataRaw['group'];
+            }
+        }
+
+        if (!empty($dataRaw['fields'])) {
+            $dataInsert['fields'] = $dataRaw['fields'];
+        }
+        return $dataInsert;
     }
 }
