82239200e7a11b9855ea5853923fc4d6b5c7297e
S-Cart <giaiphap247.com@gmail.com>
2022-07-23 10:04:07 +0700

    Update version core

--- src/Config/s-cart.php
+++ src/Config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'core'             => '7.1',
-    'core-sub-version' => '7.1.15',
+    'core-sub-version' => '7.1.16',
     'homepage'         => 'https://s-cart.org',
     'name'             => 'S-Cart',
     'github'           => 'https://github.com/s-cart/s-cart',
