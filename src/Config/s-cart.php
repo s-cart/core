4f1dcd4b8a4baf056bc8268a19ae25deb89f719e
S-Cart <giaiphap247.com@gmail.com>
2022-09-04 18:33:17 +0700

    Update 7.2 beta

--- src/Config/s-cart.php
+++ src/Config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'core'             => '7.1',
-    'core-sub-version' => '7.1.21',
+    'core'             => '7.2',
+    'core-sub-version' => '7.2-beta',
     'homepage'         => 'https://s-cart.org',
     'name'             => 'S-Cart',
     'github'           => 'https://github.com/s-cart/s-cart',
