e259197732cf36c05421f3acd7459c826a76fb51
S-Cart <giaiphap247.com@gmail.com>
2023-01-10 22:39:09 +0700

    Add function check limit size upload from system

--- src/Library/Helpers/file.php
+++ src/Library/Helpers/file.php
@@ -288,3 +288,71 @@ if (!function_exists('sc_path_download_render') && !in_array('sc_path_download_r
         }
     }
 }
+
+if (!function_exists('sc_convertPHPSizeToBytes') && !in_array('sc_convertPHPSizeToBytes', config('helper_except', []))) {
+    /**
+    * This function transforms the php.ini notation for numbers (like '2M') to an integer (2*1024*1024 in this case)
+    * 
+    * @param string $sSize
+    * @return integer The value in bytes
+    */
+    function sc_convertPHPSizeToBytes(string $sSize):int
+    {
+        $sSuffix = strtoupper(substr($sSize, -1));
+        if (!in_array($sSuffix,array('P','T','G','M','K'))){
+            return (int)$sSize;  
+        } 
+        $iValue = substr($sSize, 0, -1);
+        switch ($sSuffix) {
+            case 'P':
+                $iValue *= 1024;
+                // Fallthrough intended
+            case 'T':
+                $iValue *= 1024;
+                // Fallthrough intended
+            case 'G':
+                $iValue *= 1024;
+                // Fallthrough intended
+            case 'M':
+                $iValue *= 1024;
+                // Fallthrough intended
+            case 'K':
+                $iValue *= 1024;
+                break;
+        }
+        return (int)$iValue;
+    }
+}
+
+if (!function_exists('sc_getMaximumFileUploadSize') && !in_array('sc_getMaximumFileUploadSize', config('helper_except', []))) {
+    /**
+    * This function returns the maximum files size that can be uploaded 
+    * in PHP
+    * @returns  File size in bytes
+    **/
+    function sc_getMaximumFileUploadSize($unit = null)
+    {
+        $valueUnit = 1;
+        switch ($unit) {
+            case 'P':
+                $valueUnit = 1024 * 1024 * 1024 * 1024 * 1024;
+                break;
+            case 'T':
+                $valueUnit = 1024 * 1024 * 1024 * 1024;
+                break;
+            case 'G':
+                $valueUnit = 1024 * 1024 * 1024;
+                break;
+            case 'M':
+                $valueUnit = 1024 * 1024;
+                break;
+            case 'K':
+                $valueUnit = 1024;
+                break;
+            default:
+                $valueUnit = 1;
+                break;
+        }
+        return min(sc_convertPHPSizeToBytes(ini_get('post_max_size')), sc_convertPHPSizeToBytes(ini_get('upload_max_filesize')))/ $valueUnit;
+    }
+}
