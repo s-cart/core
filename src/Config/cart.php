6b343759eedf6da3da4764e58256bc1935ee3324
S-Cart <tech@s-cart.org>
2023-07-10 05:11:33 +0700

    Add config for productsLastView

--- src/Config/cart.php
+++ src/Config/cart.php
@@ -5,6 +5,7 @@ return [
         'cart' => env('SC_CART_EXPIRE_CART', 7), //days
         'wishlist' => env('SC_CART_EXPIRE_WISHLIST', 30), //days
         'compare' => env('SC_CART_EXPIRE_COMPARE', 30), //days
+        'lastview' => env('SC_CART_EXPIRE_PRODUCT_LASTVIEW', 30), //days
     ],
     'process' => [
         'other_fee' => [
