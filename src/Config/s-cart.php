92fb816c4006cb15f94e63b30e3ad7432e419b49
S-Cart <tech@s-cart.org>
2024-03-20 20:29:21 +0700

    Update Laravel 11

--- src/Config/s-cart.php
+++ src/Config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
-    'core'             => '8.1',
-    'core-sub-version' => '8.1.18',
+    'core'             => '9.0',
+    'core-sub-version' => '9.0.0',
     'homepage'         => 'https://s-cart.org',
     'name'             => 'S-Cart',
     'github'           => 'https://github.com/s-cart/s-cart',
