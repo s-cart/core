f1bf0a34e5adf0907a51526f69aef637340ccd0a
S-Cart <tech@s-cart.org>
2024-03-23 21:17:43 +0700

    Update

--- src/Config/s-cart.php
+++ src/Config/s-cart.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'core'             => '9.0',
-    'core-sub-version' => '9.0.0',
+    'core-sub-version' => '9.0.1',
     'homepage'         => 'https://s-cart.org',
     'name'             => 'S-Cart',
     'github'           => 'https://github.com/s-cart/s-cart',
